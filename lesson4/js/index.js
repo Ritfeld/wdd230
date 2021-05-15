84832d6a7a5e4f1786e30b702b2498e0592929c0
Use your GitHub username <Enter your email address>
2021-05-15 19:24:03 -0400

    save

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -1,7 +1,7 @@
 const navbutton = document.querySelector('.nav');
 const mainnav = document.querySelector('.navigation')
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+navbutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
