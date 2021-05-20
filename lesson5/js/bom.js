37f3814d870391a717af569a91e766aecfab360f
Use your GitHub username <Enter your email address>
2021-05-20 09:04:16 -0400

    save

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -1,6 +1,6 @@
 const fav = document.querySelector('#fav');
 const button = document.querySelector('button');
-const output = document.querySelector('.listul');
+const output = document.querySelector('.list');
 
 button.addEventListener('click', () => {
   if (fav.value != '') {
