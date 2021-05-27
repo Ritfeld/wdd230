79e55fda2baaec03746eb709c22c70754711b567
Use your GitHub username <Enter your email address>
2021-05-27 11:56:30 -0400

    update image photo

--- lesson6/js/banner6.js
+++ lesson6/js/banner6.js
@@ -7,3 +7,14 @@ if (thedate.getDate() ==6) {
     document.querySelector("#banner").style.display = "block"
     
 }
+
+const thisday = new Date();
+const dayNumber = thisday.getDay();
+
+
+if (dayNumber == 5) {
+    document.getElementById("banner").classList.add("showme");
+}
+else {
+    document.getElementById("banner").classList.add("hideme");
+}
