d6d6e0b29f84697d5bc8404e66bb71192f958228
Use your GitHub username <Enter your email address>
2021-05-29 19:16:32 -0400

    update error

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,12 +1,26 @@
-var a = parseFloat(document.getElementById('temp').value);
-var b = parseFloat(document.getElementById('windspeed').value);
-var wc = windChill (a,b)
-document.getElementById('windchill').innerHTML = wc;
-
 function windChill(tempF,speed) 	
 {
 var a = Math.pow(speed,.16);
-return result = (35.74 + (.6215 * tempF) - (35.75* a) + (.4275 * tempF * a)).toFixed(2) ;
+return  (35.74 + (.6215 * tempF) - (35.75* a) + (.4275 * tempF * a)).toFixed(2);
+}
+var  a = parseFloat(document.getElementById('temp').innerHTML);
+var b = parseFloat(document.getElementById('windspeed').innerHTML);
+console.log(a)
+var wc = windChill(a,b)
+document.getElementById('windchill').innerHTML = wc;
 
 
+
+if (temp <= 50) {
+    if (windSpeed <= 3) {
+        windChill = temperature;
+    }
+    else {
+        windChill = 35.74 + 0.6215*temperature + 
+                    (0.4275*temperature - 35.75)*Math.pow(windSpeed, 0.16);
+    }
+}
+else {
+    alert("Wind chill is defined only if temperature <= 50.");
+    windChill = NaN;
 }
