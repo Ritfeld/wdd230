6e12b445b6d1fd937d2e13b99696842f5a623dd2
Use your GitHub username <Enter your email address>
2021-06-24 12:35:17 -0400

    save

---
+++ lesson10/js/forecast2.js
@@ -0,0 +1,24 @@
+let cityId = 5604473;
+let appid = 'e954957c737b89d0338f8fd5f015fe9d';
+
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=${cityId}&units=imperial&appid=${key}";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    let day = 0;
+    const dayofweek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    //reduce the list array to the five forecast.....40
+    const fiveDayForcast = jsObject.list.filter( forecast => forecast.dt_txt.includes('12:00:00'));
+    console.log(fiveDayForcast);
+
+    // loop through each of the forecast days
+    fiveDayForcast.forEach( x => {
+        let d = new Date(x.dt_txt);
+        //console.log(d);
+        document.getElementById(' dayofweek${day+1}').textContent = dayofweek[d.getDay()];
+        document.getElementById('forecast${day+1}').textContent = x.main.temp;
+        day++
+    });
+});
--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,15 +1,24 @@
-https://api.openweathermap.org/data/2.5/weather?id=${cityId}&units=imperial&appid=${key}
-const apiURL = "...";
+let cityId = 5604473;
+let appid = 'e954957c737b89d0338f8fd5f015fe9d';
+
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=${cityId}&units=imperial&appid=${key}";
+
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-  });
+    const temperature = document.querySelector('#temperature');
+    temperature.textContent = jsObject.main.temp.toFixed(0);
+    const currently = document.querySelector('#currently');
+    const desc = jsObject.weather[0].description;
+    currently.innerHTML = '<strong>${desc.toUpperCase()}</strong>';
 
-  document.getElementById('current-temp').textContent = jsObject.main.temp;
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
 
-  const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-const desc = jsObject.weather[0].description;  // note how we reference the weather array
-document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-document.getElementById('icon').setAttribute('alt', desc);
+
+    const imagesrc = 'https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+});
