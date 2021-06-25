36566749b1018cb414fea76dc695917b257fc5a6
Use your GitHub username <Enter your email address>
2021-06-25 02:38:40 -0400

    save

--- lesson10/js/forecast2.js
+++ lesson10/js/forecast2.js
@@ -10,15 +10,19 @@ fetch(apiURL)
     let day = 0;
     const dayofweek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     //reduce the list array to the five forecast.....40
-    const fiveDayForcast = jsObject.list.filter( forecast => forecast.dt_txt.includes('12:00:00'));
+    const fiveDayForcast = jsObject.list.filter( forecast => forecast.dt_txt.includes('18:00:00'));
     console.log(fiveDayForcast);
 
     // loop through each of the forecast days
     fiveDayForcast.forEach( x => {
         let d = new Date(x.dt_txt);
-        //console.log(d);
-        document.getElementById(' dayofweek${day+1}').textContent = dayofweek[d.getDay()];
-        document.getElementById('forecast${day+1}').textContent = x.main.temp;
+        console.log(d);
+        document.getElementById('day' + day).textContent = dayofweek[d.getDay()];
         day++
-    });
+    })
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
 });
