fcb431cc04598deac1372078ffefb9ede6a3c84e
Use your GitHub username <Enter your email address>
2021-06-26 20:14:20 -0400

    save

--- lesson10/js/forecast2.js
+++ lesson10/js/forecast2.js
@@ -2,22 +2,22 @@ let cityId = 5604473;
 let appid = 'e954957c737b89d0338f8fd5f015fe9d';
 
 const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=e954957c737b89d0338f8fd5f015fe9d";
-console.log('test');
+//console.log('test');
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
+   // console.log(jsObject);
     let day = 0;
     const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     //reduce the list array to the five forecast.....40
     const fiveDayForcast = jsObject.list.filter( forecast => forecast.dt_txt.includes('18:00:00'));
-    console.log(fiveDayForcast);
+    //console.log(fiveDayForcast);
 
     // loop through each of the forecast days
     fiveDayForcast.forEach( x => {
         let d = new Date(x.dt_txt);
-        console.log(d);
-        console.log(`dayofWeek${day+1}`)
+        //console.log(d);
+       // console.log(`dayofWeek${day+1}`)
        document.getElementById(`dayofWeek${day+1}`).textContent = dayofWeek[d.getDay()];
         document.getElementById(`forecast${day+1}`).textContent = x.main.temp;
         const imagesrc = 'https://openweathermap.org/img/w/' + x.weather[0].icon + '.png';  // note the concatenation
