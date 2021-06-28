0f45d35c8fac845c69bf0d9e9723725b11d6e686
Use your GitHub username <Enter your email address>
2021-06-28 17:09:56 -0400

    save

--- lesson10/js/forecast2.js
+++ lesson10/js/forecast2.js
@@ -1,12 +1,15 @@
 let cityId = 5604473;
 let appid = 'e954957c737b89d0338f8fd5f015fe9d';
 
-const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=e954957c737b89d0338f8fd5f015fe9d";
+const apiURL = `api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${appid
+}`;
+
 //console.log('test');
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
    // console.log(jsObject);
+   console.log(jsObject);
     let day = 0;
     const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     //reduce the list array to the five forecast.....40
