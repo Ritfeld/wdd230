425266ab3fa7ee457321b6b0dba323a245a0b6c1
Use your GitHub username <Enter your email address>
2021-06-30 18:00:40 -0400

    save

--- lesson10/js/towns.js
+++ lesson10/js/towns.js
@@ -24,13 +24,16 @@ fetch(requestURL)
 
         h2.textContent = towns[i].name;
         h3.textContent = towns[i].motto;
+        h2.textContent =towns[i].events;
         p2.textContent = "Year Founded: " + towns[i].yearFounded;
         p3.textContent = "Current Population: " + towns[i].currentPopulation;
         p4.textContent = "Average Rainfall: " + towns[i].averageRainfall + " inches";
         image.setAttribute('src', "images/" + towns[i].photo);
         image.setAttribute('alt', "photo of "+ towns[i].name);
         image.setAttribute('class', 'townimage');
+        data.setAttribute('instant', 'towndata');
         data.setAttribute('class', 'towndata');
+        data.setAttribute('class', 'instant');
 
         data.appendChild(h2);
         data.appendChild(h3);
@@ -39,6 +42,9 @@ fetch(requestURL)
         data.appendChild(p4);
         town.appendChild(data);
         town.appendChild(image)
+        town.appendChild(p2);
+        data.appendChild(p2);
+
 
         document.querySelector('div.towns').appendChild(town);
         
