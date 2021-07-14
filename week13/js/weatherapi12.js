5ecb79c38b3319f0c73da5f571a977d4efc7796f
Use your GitHub username <Enter your email address>
2021-07-14 17:46:59 -0400

    save city page

--- week13/js/weatherapi12.js
+++ week13/js/weatherapi12.js
@@ -1,7 +1,7 @@
 //let cityId = 3383330;
 //let appid = 'e954957c737b89d0338f8fd5f015fe9d';
 
-const apiURL1 = "https://api.openweathermap.org/data/2.5/weather?id=3383330&units=imperial&appid=e954957c737b89d0338f8fd5f015fe9d";
+const apiURL1 = "https://api.openweathermap.org/data/2.5/onecall?lat=5.839398&lon=-55.199089&units=imperial&appid=e954957c737b89d0338f8fd5f015fe9d";
 
 fetch(apiURL1)
   .then((response) => response.json())
@@ -11,9 +11,9 @@ fetch(apiURL1)
     const description = document.querySelector('#desc' );
     const windspeed = document.querySelector('#windSpeed' );
     const humidity = document.querySelector('#hum' );
-    temperature.textContent = jsObject.main.temp;
-    description.textContent = jsObject.weather[0].description;
-    windspeed.textContent = jsObject.wind.speed;
-    humidity.textContent = jsObject.main.humidity;
+    temperature.textContent = jsObject.current.temp;
+    description.textContent = jsObject.current.weather[0].description;
+    windspeed.textContent = jsObject.current.wind_speed;
+    humidity.textContent = jsObject.current.humidity;
 
   });
